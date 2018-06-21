dd5d70a5b1ba60a36e6210ceb3991748bd0c782c
leexiaosong908 <leexiaosong908@gmail.com>
2018-06-21 20:08:52 +0800

    utils.py

--- movie/utils.py
+++ movie/utils.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import os
+import time
 from django.core.wsgi import get_wsgi_application
 sys.path.extend(['/home/sg/wxmovie',])
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','wxmovie.settings')
@@ -22,20 +23,117 @@ header = {
     'Referer':'http://www.friok.com/'
 }
 
+def get_movie_from_friok2():
+    list_urls = []
+    detail_ids = []
+
+    # 获取总页数
+    gaoq_url = 'http://www.friok.com/category/gaoq'
+    gaoq_data = requests.get(gaoq_url,headers = header,timeout=30).content
+    gaoq_soup = BeautifulSoup(gaoq_data,'html5lib')
+    pagenums = gaoq_soup.select('.page-numbers')
+    pagemax = int(pagenums[-2].text)
+    #print(pagemax)
+
+    # 生成列表页的url
+    for page in range(1,pagemax):
+        #if page % 2 == 0:
+        host = 'http://www.friok.com/category/gaoq/page/{0}'.format(page)
+        list_urls.append(host)
+    #print(list_urls)
+
+    # 获取资源id
+    # for url in list_urls:
+    #     try:
+    #         detail = requests.get(url,headers = header,timeout=20).content
+    #         soup = BeautifulSoup(detail,'html5lib')
+    #         ds = soup.select('h2 > a')
+    #         if len(ds) > 0:
+    #             for d in ds:
+    #                 d = d.get("href")
+    #                 d = d.split('m/')[1].split('.')[0]
+    #                 #print(d)
+    #                 if d is not None:
+    #                     detail_ids.append(d)
+    #                 else:
+    #                     continue
+    #             list_urls.remove(url)
+    #         else:
+    #             print('资源获取失败:%s'%url)
+    #             continue
+    #     except BaseException as e:
+    #         print('访问出错:%s %s'%(e,url))
+    #         continue
+
+    # print('资源数量:%d'%len(detail_ids))
+    # print('资源剩余:%d \n %s'%(len(list_urls),list_urls))
+
+    # 获取资源id
+    while len(list_urls) > 0:
+        print('获取:%d 剩余:%d页'%(len(detail_ids),len(list_urls)))
+        try:
+            url = list_urls[-1]
+            detail = requests.get(url,headers = header,timeout=20).content
+            soup = BeautifulSoup(detail,'html5lib')
+            ds = soup.select('h2 > a')
+            if len(ds) > 0:
+                for d in ds:
+                    d = d.get("href")
+                    d = d.split('m/')[1].split('.')[0]
+                    #print(d)
+                    if d is not None:
+                        detail_ids.append(d)
+                    else:
+                        continue
+                list_urls.remove(url)
+            else:
+                print('资源获取失败:%s'%url)
+                continue
+        except BaseException as e:
+            print('访问出错:%s %s'%(e,url))
+            continue
+    else:
+        print('一共 %d 资源'%len(detail_ids))
+        print(detail_ids)
+
+    # 获取下载页信息
+    for n in detail_ids:
+        try:
+            down_url = 'http://www.friok.com/download.php?id='+n
+            down_data = requests.get(down_url,headers = header, timeout=20).content
+            dsoup = BeautifulSoup(down_data,'html5lib')
+            name = dsoup.title.get_text()[:-4]
+            link = dsoup.select_one('div.list > a').get('href')
+            passwd = dsoup.select('div.desc > p')[1].get_text().split(': ')[-1]
+            result = Movie.objects.get_or_create(
+                title = name,
+                link = link,
+                passwd = passwd
+            )
+            print('[*]完成数据写入:',name,link,passwd)
+            detail_ids.remove(n)
+        except Exception as e:
+            print('[-]数据写入错误%s %s'%(down_url,e))
+            continue
+
+
 def get_movie_from_friok(page = 1):
     host = 'http://www.friok.com/category/gaoq/page/{0}'.format(page)
     try:
         wb = requests.get(host,headers = header,timeout=10).content
         soup = BeautifulSoup(wb,'html5lib')
+
+        nums_list = []
+        movie_nums = soup.select("h2 > a")
+        for m in movie_nums:
+            m = m.get("href")[21:-5]
+            nums_list.append(m) 
     except BaseException as e:
         print('访问出错:',e)
     
-    nums_list = []
-    movie_nums = soup.select("h2 > a")
-    for m in movie_nums:
-        m = m.get("href")[21:-5]
-        nums_list.append(m)
-    video_list = []
+    print(nums_list)
+
+
     for n in nums_list:
         down_url = 'http://www.friok.com/download.php?id='+n
 
@@ -64,6 +162,7 @@ def get_movie_from_friok(page = 1):
     
 if __name__ == '__main__':
     #get_movie_from_friok()
-    for i in range(52,121):
-        print('当前第{}页'.format(i))
-        get_movie_from_friok(page=i)
+    # for i in range(52,121):
+    #     print('当前第{}页'.format(i))
+    #     get_movie_from_friok(page=i)
+    get_movie_from_friok2()
